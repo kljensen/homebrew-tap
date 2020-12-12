7b5cb26e17429295060f3e5e55acbf5305aef663
Kyle L. Jensen <kljensen@gmail.com>
2020-12-12 10:58:53 -0500

    Fix formula for semiuniq

--- Formula/semiuniq.rb
+++ Formula/semiuniq.rb
@@ -2,8 +2,8 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Semiuniq < Formula
-  desc "A uniq-like tool for removing nearby repeated lines in a file""
-  homepage ""
+  desc "A uniq-like tool for removing nearby repeated lines in a file"
+  homepage "https://github.com/kljensen/semiuniq/"
   url "https://github.com/kljensen/semiuniq/archive/v0.1.5.tar.gz"
   sha256 "45d6e9010995f1d531de6a5324e6f5e9d714c9b3334f4dee7b1f0f9ca7a11800"
   license "Unlicense"
