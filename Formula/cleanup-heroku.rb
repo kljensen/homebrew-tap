9874ef15e77170702a9762b55b43bf65d6b47f9c
Kyle L. Jensen <kljensen@gmail.com>
2020-12-11 22:12:15 -0500

    Add license

--- Formula/cleanup-heroku.rb
+++ Formula/cleanup-heroku.rb
@@ -5,7 +5,7 @@ class CleanupHeroku < Formula
   homepage "https://github.com/kljensen/cleanup-heroku/"
   url "https://github.com/kljensen/cleanup-heroku/archive/v0.1.1.tar.gz"
   sha256 "1f110adf3f1260fcea133e028e5ead815c3819c3e85ea1479bf448f9ba468b08"
-  license ""
+  license "Unlicense"
 
   def install
     bin.install "cleanup-heroku.sh" => "cleanup-heroku"
